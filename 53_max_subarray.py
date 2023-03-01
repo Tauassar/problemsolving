d91c69ddd410a1eb6d2b0d76188396da4d7fc02b
t.tatiyev <t.tatiyev@trafficwave.kz>
2023-03-01 23:07:02 +0600

    linear solution finished

--- 53_max_subarray.py
+++ 53_max_subarray.py
@@ -3,5 +3,51 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        ...
+        has_pos = False
+        min_pos = None
+        for num in nums:
+            if num >= 0:
+                has_pos = True
 
+            if has_pos:
+                if min_pos is None or 0 <= num < min_pos:
+                    min_pos = num
+
+        if not has_pos:
+            return max(nums)
+
+        seq_list = []
+        prev_sum = None
+        for num in nums:
+            if prev_sum is None:
+                if num < min_pos:
+                    continue
+                else:
+                    prev_sum = num
+            else:
+                if num >= min_pos:
+                    prev_sum += num
+                else:
+                    if prev_sum + num < 0:
+                        seq_list.append(prev_sum)
+                        prev_sum = 0
+                    else:
+                        seq_list.append(prev_sum)
+                        prev_sum += num
+
+        seq_list.append(prev_sum)
+
+        max_sum = None
+        for i in seq_list:
+            if max_sum is None:
+                max_sum = i
+            else:
+                if max_sum < i:
+                    max_sum = i
+        return max_sum
+
+
+res = Solution().maxSubArray([5,4,-1,7,8])
+
+print(f'Expected: {6}')
+print(f'Received {res}')
